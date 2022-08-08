35c95077ad4f768de42d0c3b3055a5bfb46ae544
Your Name <you@example.com>
2022-08-08 11:13:44 +0530

    Exception program done

---
+++ src/Exception/ExceptionDemo.java
@@ -0,0 +1,23 @@
+package Exception;
+
+public class ExceptionDemo
+{
+	
+  public static void main(String[]args)
+  {
+	  int a=20,b=0,c;
+	  
+	  try
+	  {
+		   c=a/b;
+		   System.out.println(c);
+	  }
+	  catch(ArithmeticException e)
+	  {
+		  System.out.println("Can't divide by zero");
+	  }
+	  
+	  
+  }
+    
+}
